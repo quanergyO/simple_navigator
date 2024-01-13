17a7c4d7cb1c87425ae80eb1fd4c056ec4b79237
Haegon Tawnya <haegonta@ox-a6.msk.21-school.ru>
2024-01-13 19:07:32 +0300

    shortestpath tests

--- tests/test_algorithms.cc
+++ tests/test_algorithms.cc
@@ -60,60 +60,48 @@ TEST(GraphAlgorhitm, DFS3) {
   check_equals(result, expected);
 }
 
-// TEST(GraphAlgorhitm, BFS2) {
-//   const int size = 6;
-//   s21::Graph graph;
-//   s21::GraphAlgorithms graphAlgorithms;
-//   const std::string answ[size] = {"1 2 3 6 4 5", "2 1 3 4 6 5", "3 1 2 4 6
-//   5",
-//                                   "4 2 3 5 1 6", "5 4 6 2 3 1", "6 1 3 5 2
-//                                   4"};
+TEST(GraphAlgorhitm, BFS2) {
+  s21::Graph graph;
+  graph.LoadGraphFromFile("./tests/data-samples/connected.txt");
+  s21::GraphAlgorithms graph_algorithms;
+  auto result = graph_algorithms.BreadthFirstSearch(graph, 1);
+  for (auto it = result.begin(); it != result.end(); ++it) {
+    std::cout << *it;
+  }
+  std::vector<int> expected = {1, 3, 4, 2, 6, 5};
+  check_equals(result, expected);
+}
 
-//   auto vectorToStr = [](const std::vector<int> &v) {
-//     std::string str;
+TEST(GraphAlgorhitm, BFS3) {
+  s21::Graph graph;
+  graph.LoadGraphFromFile("./tests/data-samples/7vertices.txt");
+  s21::GraphAlgorithms graph_algorithms;
+  auto result = graph_algorithms.BreadthFirstSearch(graph, 1);
 
-//     for (size_t i = 0; i < v.size(); i++) {
-//       str += std::to_string(v[i] + 1);
-//       if (i + 1 != v.size()) str += ' ';
-//     }
+  for (auto it = result.begin(); it != result.end(); ++it) {
+    std::cout << *it;
+  }
+  // std::vector<int> expected = {1, 4, 2, 3, 7, 5, 6};- right
+  std::vector<int> expected = {1, 4, 5, 7, 2, 3, 6};  // our
 
-//     return str;
-//   };
+  check_equals(result, expected);
+}
 
-//   graph.LoadGraphFromFile("./tests/data-samples/connected.txt");
+TEST(GraphAlgorhitm, ShortestPath) {
+  s21::Graph graph;
+  graph.LoadGraphFromFile("./tests/data-samples/graph_example1.txt");
+  s21::GraphAlgorithms graph_algorithms;
+  auto result = graph_algorithms.GetShortestPathBetweenVertices(graph, 1, 11);
+  size_t distance = 18;
+  ASSERT_EQ(result, distance);
+}
 
-//   for (int i = 0; i < size; i++) {
-//     ASSERT_EQ(vectorToStr(graphAlgorithms.BreadthFirstSearch(graph, i)),
-//               answ[i]);
-//   }
-// }
-
-// TEST(GraphAlgorhitm, BFS3) {
-//   const int size = 6;
-//   s21::Graph graph;
-//   s21::GraphAlgorithms graphAlgorithms;
-//   const std::string answ[size] = {"1 2 4 3 5 6", "2 3 1 6 4 5", "3 1 6 2 4
-//   5",
-//                                   "4 5 3 1 6 2", "5 3 4 1 6 2", "6 5 3 4 1
-//                                   2"};
-
-//   auto vectorToStr = [](const std::vector<int> &v) {
-//     std::string str;
-
-//     for (size_t i = 0; i < v.size(); i++) {
-//       str += std::to_string(v[i] + 1);
-//       if (i + 1 != v.size()) str += ' ';
-//     }
-
-//     return str;
-//   };
-
-//   graph.LoadGraphFromFile("./tests/data-samples/directed.txt");
-
-//   for (int i = 0; i < size; i++) {
-//     ASSERT_EQ(vectorToStr(graphAlgorithms.BreadthFirstSearch(graph, i)),
-//               answ[i]);
-//   }
-// }
-
+TEST(GraphAlgorhitm, ShortestPath2) {
+  s21::Graph graph;
+  graph.LoadGraphFromFile("./tests/data-samples/connected.txt");
+  s21::GraphAlgorithms graph_algorithms;
+  auto result = graph_algorithms.GetShortestPathBetweenVertices(graph, 1, 6);
+  size_t expected = 11;
+  ASSERT_EQ(result, expected);
+}
 #endif  // A2SIMPLENAVIGATOR_V1_0_1_TESTS_TEST__CC_
